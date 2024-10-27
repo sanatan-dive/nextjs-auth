cbc068e73e5ee71d2ba59c1a5bb954629f945829
sanatan-dive <sanatansharma352@gmail.com>
2024-10-27 16:54:38 +0530

    fixes

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -19,7 +19,7 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       });
     }
 
-    var transport = nodemailer.createTransport({
+    const transport = nodemailer.createTransport({
       host: "sandbox.smtp.mailtrap.io",
       port: 2525,
       auth: {
